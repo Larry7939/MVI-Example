5119e3d5e8648cfb98882112709bcb72c6f66570
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2025-02-09 15:14:10 +0900

    InitialCommit

---
+++ app/src/main/java/com/oneviewonestatepatternexample/oneviewonestatepatternexample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.oneviewonestatepatternexample.oneviewonestatepatternexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.oneviewonestatepatternexample.oneviewonestatepatternexample.ui.theme.OneViewOneStatePatternExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OneViewOneStatePatternExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android1",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OneViewOneStatePatternExampleTheme {
+        Greeting("Android")
+    }
+}
